f753adb81c31e145ff489944ef7279eb74ec8700
LimKentPhang <kentphang1990@gmail.com>
2025-03-17 21:53:35 +0800

    Done Exercises1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function (string, repeatTimes) {
+  //if (repeatTimes < 0) return (string = "ERROR");
+  let strings = "";
 
+  for (let i = 1; i <= repeatTimes; i++) {
+    strings += string;
+  }
+  return strings;
 };
 
 // Do not edit below this line
