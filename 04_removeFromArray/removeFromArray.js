76d1104329b5029d915f4877da3c74e888152df9
LimKentPhang <kentphang1990@gmail.com>
2025-03-18 16:42:41 +0800

    Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+/*const removeFromArray = function (arr =[], ...args) {
+  return arr.filter((item) => !args.includes(item));
+}; */
+
+const removeFromArray = function (arr = [], ...args) {
+  const tempArray = [];
+
+  arr.forEach((items) => {
+    if (!args.includes(items)) {
+      tempArray.push(items);
+    }
+  });
+  return tempArray;
 };
 
 // Do not edit below this line
